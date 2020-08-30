22a4585137cb91dd8881905139026cb49dcb9412
DragonSky <38320524+DragonSky2357@users.noreply.github.com>
2020-08-30 22:14:43 +0900

    Update Naver_Webtoon_Make_JSON.py

--- Naver_Webtoon_Make_JSON.py
+++ Naver_Webtoon_Make_JSON.py
@@ -21,13 +21,13 @@ for index in range(len(days)):
         webtoonDataOne = data.select('div > a')[0]
         webtoonDataTwo = data.select('dl > dd')
 
-        title = webtoonDataOne['title']
-        img = webtoonDataOne.select('img')[0]['src']
-        link = webtoonDataOne['href']
+        title = webtoonDataOne['title'] # 제목
+        img = webtoonDataOne.select('img')[0]['src'] # 이미지
+        link = webtoonDataOne['href'] # 링크
         id = link[link.find('titleId=') + len('titleId=')
-                            :link.find("&", link.find('titleId=') + len('titleId='))]
-        author = webtoonDataTwo[0].select('a')[0].text
-        grade = webtoonDataTwo[1].select('div > strong')[0].text
+                            :link.find("&", link.find('titleId=') + len('titleId='))] # 고유 아이디
+        author = webtoonDataTwo[0].select('a')[0].text # 작가
+        grade = webtoonDataTwo[1].select('div > strong')[0].text # 시청연령
 
         webtoonList["webtoonList"].append(
             {"id": id, "title": title, "author": author, "day": days[index], "link": link, "img": img, "grade": grade})
@@ -35,6 +35,6 @@ for index in range(len(days)):
     # print(title, img, link, Id, author, grade)
 
 jsonString = json.dumps(webtoonList, ensure_ascii=False, indent=4)
-f = open('test.json', "w")
+f = open('webtoon.json', "w")
 f.write(jsonString)
 f.close()
